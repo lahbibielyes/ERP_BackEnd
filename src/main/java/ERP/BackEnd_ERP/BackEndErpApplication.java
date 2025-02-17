083feb2834c91f979f2fe3f5465fd12a6380640b
mechergui med aziz <mechergui.aziz843@gmail.com>
2025-02-17 14:34:14 +0100

    starting

---
+++ src/main/java/ERP/BackEnd_ERP/BackEndErpApplication.java
@@ -0,0 +1,13 @@
+package ERP.BackEnd_ERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ERP/BackEnd_ERP/BackEndErpApplicationTests.java
@@ -0,0 +1,13 @@
+package ERP.BackEnd_ERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
